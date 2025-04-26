d7bec108d94c1ec3bd04947013c9d9b8e4fcf6ec
geraldy.miero <geraldy.miero@maucash.id>
2025-04-26 16:39:07 +0700

    update: add connection to db and add create method

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 const express = require('express')
-
+const mongoose = require('mongoose')
+const { body, validationResult } = require('express-validator')
 const router = express.Router()
+const Registration = mongoose.model('Registration')
 
+// req = objects full of data or params
+// res = objects full of methods for user
+// next = if not want to send data back or pass the req off for something else
 router.get('/', (req, res) => {
-    res.send('Hello World')
+    res.render('form', { title: 'Registration Form' })
+})
+
+router.post('/',
+    [
+        body('name').trim().isLength({ min: 1 }).withMessage('Please enter a name'),
+        body('email').trim().isLength({ min: 1 }).withMessage('Please enter an email'),
+    ],
+    (req, res) => {
+    console.log(req.body)
+    const errors = validationResult(req)
+    if (errors.isEmpty()) {
+        const registration = new Registration(req.body)
+        registration.save().then(() => {
+            res.send('Data submitted')
+        }).catch(() => {
+            res.send('Sorry, something went wrong')
+        })
+    } else {
+        res.render('form', {
+            title: 'Registration Form',
+            errors: errors.array(),
+            data: req.body,
+        })
+    }
 })
 
 module.exports = router
